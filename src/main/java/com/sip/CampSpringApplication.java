e115c2f62d5c1ba95c8d619bd95bfeac66d4e613
moahmed amine mezghich <git@mezghich.com>
2023-03-26 14:16:15 +0100

    update 1 sur la branche test

--- src/main/java/com/sip/CampSpringApplication.java
+++ src/main/java/com/sip/CampSpringApplication.java
@@ -24,6 +24,7 @@ public class CampSpringApplication {
 		//new File(ArticleController.uploadDirectory).mkdir();
 		Path path = Paths.get(ArticleController.uploadDirectory);
 		System.out.println("Camp Devops Started");
+		System.out.println("Instruction 5 sur la branche test");
 		try{
 			Files.createDirectory(path);
 		}
